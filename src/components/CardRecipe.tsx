964acd083256f020b942c7c65d038f083f7e832a
Chatbordin <klinscea@gmail.com>
2025-09-04 15:05:56 +0700

    Update CardRecipe.tsx

--- src/components/CardRecipe.tsx
+++ src/components/CardRecipe.tsx
@@ -1,7 +1,8 @@
 import { CardRecipeProps } from '@/app/page'
+import { Heart } from 'lucide-react'
+import Image from 'next/image'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Card, CardContent, CardFooter } from './ui/card'
-import Image from 'next/image'
 
 const CardRecipe = ({
   name,
@@ -10,59 +11,119 @@ const CardRecipe = ({
   difficulty,
   cookingDuration,
   user,
-}: CardRecipeProps) => (
-  <Card className='w-[276px] h-[390px]'>
-    <div>
-      <div className='h-[158px] relative rounded-t-lg pb-4'>
-        {imageUrl ? (
-          <Image
-            unoptimized
-            priority
-            src={imageUrl}
-            alt={`${name} image`}
-            fill
-            sizes='(max-width: 276px) 100vw, 276px'
-          />
-        ) : (
-          <Image
-            unoptimized
-            priority
-            src={'/images/istockphoto-1396814518-612x612.jpg'}
-            alt={`${name} image`}
-            fill
-            sizes='(max-width: 276px) 100vw, 276px'
-          />
-        )}
-      </div>
-      <div>
-        <CardContent>
-          <h1 className='font-bold'>{name}</h1>
-          <p className='text-secondary line-clamp-3'>{description}</p>
-        </CardContent>
-      </div>
-    </div>
+}: CardRecipeProps) => {
+  let difficultyTh = ''
+  switch (difficulty.name) {
+    case 'Easy':
+      difficultyTh = 'ง่าย'
+      break
+    case 'Medium':
+      difficultyTh = 'ปานกลาง'
+      break
+    case 'Hard':
+      difficultyTh = 'ยาก'
+      break
+    default:
+      difficultyTh = difficulty.name
+  }
 
-    <div>
-      <CardFooter>
-        <div className='flex w-full item-center'>
-          <div className='flex p-1 grow'>
-            <img src='/icons/av_timer.svg' alt='av timer' />
-            <p>{difficulty.name}</p>
-          </div>
-          <div className='flex p-1 grow'>
-            <img src='/icons/level.svg' alt='level' />
-            <p>{cookingDuration.name}</p>
-          </div>
+  const nameFallback = user?.firstName
+    ? user.firstName.charAt(0).toUpperCase() +
+      user.lastName.charAt(0).toUpperCase()
+    : 'User'
+
+  return (
+    <Card className='w-[276px] h-[390px]'>
+      <div>
+        <div className='h-[158px] relative rounded-t-lg pb-4'>
+          {imageUrl ? (
+            <Image
+              unoptimized
+              priority
+              src={imageUrl}
+              alt={`${name} image`}
+              fill
+              sizes='(max-width: 276px) 100vw, 276px'
+            />
+          ) : (
+            <Image
+              unoptimized
+              priority
+              src={'/images/istockphoto-1396814518-612x612.jpg'}
+              alt={`${name} image`}
+              fill
+              sizes='(max-width: 276px) 100vw, 276px'
+            />
+          )}
         </div>
-        <div>
-          <Avatar>
-            <AvatarImage src='https://github.com/shadcn.png' />
-            <AvatarFallback>{user?.firstName}</AvatarFallback>
-          </Avatar>
+        <div className='p-2'>
+          <CardContent>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'flex-start',
+              }}
+            >
+              <h1 className='font-bold max-w-[200px] line-clamp-1'>{name}</h1>
+
+              <div>
+                <Heart />
+              </div>
+            </div>
+            <p className='text-secondary line-clamp-3'>{description}</p>
+          </CardContent>
         </div>
-      </CardFooter>
-    </div>
-  </Card>
-)
+      </div>
+
+      <div>
+        <CardFooter>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '100%',
+            }}
+          >
+            <div className='flex p-1 grow gap-1'>
+              <img src='/icons/av_timer.svg' alt='av timer' />
+              <p>{difficultyTh}</p>
+            </div>
+            <div
+              className='flex p-1 grow gap-1'
+              style={{
+                justifyContent: 'flex-end',
+              }}
+            >
+              <img src='/icons/level.svg' alt='level' />
+              <p>{cookingDuration.name}</p> <p>นาที</p>
+            </div>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+            }}
+          >
+            <Avatar>
+              <AvatarImage src='https://github.com/shadcn.pn' />
+              <AvatarFallback>{nameFallback}</AvatarFallback>
+            </Avatar>
+            <p
+              className='pl-2 pt-1 text-sm text-secondary'
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              {user.firstName} {user.lastName}
+            </p>
+          </div>
+        </CardFooter>
+      </div>
+    </Card>
+  )
+}
 
 export default CardRecipe
