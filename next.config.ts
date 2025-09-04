7b2f1de5763c589ef205680067a4b9b52df4eb2f
Chatbordin <klinscea@gmail.com>
2025-09-04 10:43:41 +0700

    added: new url in images/remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL('https://foodish-api.com/images/**')],
-  }
-};
+    remotePatterns: [
+      new URL('https://foodish-api.com/images/**'),
+      new URL('https://dessertsmate.com/wp-content/uploads/**'),
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
