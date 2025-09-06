8cb707a4b8fbd0c29ae62d866e8c97bfdd9f9e3c
Chatbordin <klinscea@gmail.com>
2025-09-06 14:13:11 +0700

    Added: router guard

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,15 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-    matcher: ['/my-recipe']
+  matcher: [
+    '/my-recipe',
+    '/my-recipe/:path*',
+    '/favorites',
+    '/favorites/:path*',
+    '/create-recipe',
+    '/create-recipe/:path*',
+    '/edit-recipe/:path*',
+    '/profile',
+    '/profile/:path*',
+  ],
 }
